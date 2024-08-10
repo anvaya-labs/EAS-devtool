7a61937e21326b6089cad01cf14f41935ebd09dd
Koushith B R <koushith@creatoros.co>
2024-08-11 02:47:47 +0530

    added vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { Web3Provider } from "./components/Web3Provider.tsx";
 import "./index.css";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { ThemeProvider } from "./theme/theme-provider.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
 const client = new ApolloClient({
   uri: "https://sepolia.easscan.org/graphql",
@@ -17,6 +18,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <Web3Provider>
         <ApolloProvider client={client}>
           <App />
+          <Analytics />
         </ApolloProvider>
       </Web3Provider>
     </ThemeProvider>
